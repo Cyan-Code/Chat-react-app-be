fa5b6f827aa2d22d0c7be6262cf482047e16e889
Luis Carrion <lcarrionalvarez@gmail.com>
2021-10-11 20:08:11 -0500

    generating token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const { response } = require("express")
 const Usuario = require('../models/usuario')
+const bcrypt = require('bcryptjs');
+const { generarJWT } = require("../helpers/jwt");
 
 const crearUsuario = async (req, res = response) => {
   try {
@@ -15,12 +17,22 @@ const crearUsuario = async (req, res = response) => {
       })
     }
 
-    // Encriptar contraseña
+    const usuario = new Usuario(req.body) // Instancia creada
 
+    // Encriptar contraseña
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
 
     // Guardar en la base de datos
-    const usuario = new Usuario(req.body) // Instancia creada
     await usuario.save(); // usuario grabado
+
+    // Generar JWT
+    const token = await generarJWT( usuario.id );
+
+    return res.json({
+      ok: true,
+      usuario, token
+    })
 
   } catch (error) {
     return res.status(500).json({
@@ -31,14 +43,44 @@ const crearUsuario = async (req, res = response) => {
 }
 
 const login = async (req, res) => {
-
-  
   const {email, password} = req.body;
-  res.json({
-    ok: true,
-    msg: 'login',
-    email, password
-  })
+
+  try {
+    
+    // Verificar si existe el correo
+    const usuarioDB = await Usuario.findOne({ email });
+    if (!usuarioDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'Email no encontrado'
+      });
+    }
+
+    // Validar el password
+    const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Password incorrecto'
+      });
+    }
+
+    // Generar el nuevo JWT
+    const token = await generarJWT( usuarioDB.id );
+    return res.json({
+      ok: true,
+      usuario: usuarioDB,
+      token
+    })
+
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      ok: false,
+      msg: 'hable con el administrador'
+    });
+  }
+  
 }
 
 const revalidarToken = async (req, res) => {
